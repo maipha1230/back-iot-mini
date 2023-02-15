9c8a75c96e7a0a88da562dc6ce61d66f62227886
Phathompong <maipha1230@gmail.com>
2023-02-15 13:58:58 +0700

    update front

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ app.use(express.urlencoded({ extended: true,  limit: '500mb' }))
 
 //use express static folder
 app.use('/api', express.static(path.join(__dirname, '/public')))
-
+const distPath = path.join(__dirname, '/dist');
+app.use(express.static(distPath));
 
 const db = require('./model/index.model')
 
 app.use('/api', require('./routes/index.route'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+  });
 
 app.listen(process.env.PORT || 3001, () => {
     console.log("server is running on port: "+ process.env.PORT);
